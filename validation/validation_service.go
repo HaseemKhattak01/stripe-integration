adb66aa91f51f0f4c27d441e24bb3c719cb10326
HaseemKhattak01 <haseem.khattak.hk@gmail.com>
2025-02-06 09:58:59 +0500

    making optimized code

--- validation/validation_service.go
+++ validation/validation_service.go
@@ -14,25 +14,19 @@ func NewValidationService() ValidationService {
 }
 
 func (vs *ValidationService) ValidateCardDetails(cardDetails models.CardDetails) error {
-	if err := vs.validateField(cardDetails.CardNumber, vs.isValidCardNumber, "card number"); err != nil {
-		return err
+	fields := map[string]func(string) bool{
+		"card number": vs.isValidCardNumber,
+		"expiry month": vs.isValidMonth,
+		"expiry year": vs.isValidYear,
+		"CVC": vs.isValidCVC,
 	}
-	if err := vs.validateField(cardDetails.ExpMonth, vs.isValidMonth, "expiry month"); err != nil {
-		return err
-	}
-	if err := vs.validateField(cardDetails.ExpYear, vs.isValidYear, "expiry year"); err != nil {
-		return err
-	}
-	if err := vs.validateField(cardDetails.CVC, vs.isValidCVC, "CVC"); err != nil {
-		return err
-	}
-	return nil
-}
 
-func (vs *ValidationService) ValidateCardNumber(cardNumber string) error {
-	if err := vs.validateField(cardNumber, vs.isValidCardNumber, "card number"); err != nil {
-		return err
+	for fieldName, validator := range fields {
+		if err := vs.validateField(cardDetails.GetField(fieldName), validator, fieldName); err != nil {
+			return err
+		}
 	}
+
 	return nil
 }
 
@@ -47,21 +41,17 @@ func (vs *ValidationService) validateField(value string, validator func(string) 
 }
 
 func (vs *ValidationService) isValidCardNumber(number string) bool {
-	re := regexp.MustCompile(`^\d{16}$`)
-	return re.MatchString(number)
+	return regexp.MustCompile(`^\d{16}$`).MatchString(number)
 }
 
 func (vs *ValidationService) isValidMonth(month string) bool {
-	re := regexp.MustCompile(`^(0[1-9]|1[0-2])$`)
-	return re.MatchString(month)
+	return regexp.MustCompile(`^(0[1-9]|1[0-2])$`).MatchString(month)
 }
 
 func (vs *ValidationService) isValidYear(year string) bool {
-	re := regexp.MustCompile(`^([0-9]{4}|[0-9]{2})$`)
-	return re.MatchString(year)
+	return regexp.MustCompile(`^([0-9]{4}|[0-9]{2})$`).MatchString(year)
 }
 
 func (vs *ValidationService) isValidCVC(cvc string) bool {
-	re := regexp.MustCompile(`^\d{3,4}$`)
-	return re.MatchString(cvc)
+	return regexp.MustCompile(`^\d{3,4}$`).MatchString(cvc)
 }
